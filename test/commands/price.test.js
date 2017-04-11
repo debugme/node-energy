dc30a270a6576dfd3c28548cf4dbc2d64c8ca232
Asad Razvi <debugme@hotmail.com>
2017-04-11 03:30:02 +0100

    simplify tests by leveraging jest snapshot feature

--- test/commands/price.test.js
+++ test/commands/price.test.js
@@ -11,31 +11,23 @@ describe('the price command function', () => {
 
   it('should generate a table when given valid inputs', () => {
     options.tokens = ['price', '1000']
-    const table = [
-      { supplier: 'eon', plan: 'variable', totalCost: 108.68 },
-      { supplier: 'edf', plan: 'fixed', totalCost: 111.25 },
-      { supplier: 'ovo', plan: 'standard', totalCost: 120.23 },
-      { supplier: 'bg', plan: 'standing-charge', totalCost: 121.33 }
-    ]
-    expect(command(options)).toEqual(table)
+    expect(command(options)).toMatchSnapshot()
   })
 
   it('should throw an error if wrong number of arguments were passed in', () => {
     options.tokens = ['price']
-    const expectMessage = 'Error: format is "price <annual_usage>"'
     let actualMessage = ''
     try { command(options) }
     catch (error) { actualMessage = error.toString() }
-    expect(actualMessage).toEqual(expectMessage)
+    expect(actualMessage).toMatchSnapshot()
   })
 
   it('should throw an error if invalid annual usage was passed in', () => {
     options.tokens = ['price', '10.5']
-    const expectMessage = 'Error: annual usage "10.5" is not a positive integer amount'
     let actualMessage = ''
     try { command(options) }
     catch (error) { actualMessage = error.toString() }
-    expect(actualMessage).toEqual(expectMessage)
+    expect(actualMessage).toMatchSnapshot()
   })
 
 })
--- test/commands/usage.test.js
+++ test/commands/usage.test.js
@@ -11,57 +11,52 @@ describe('the usage command function', () => {
 
   it('should return correct usage for constant plan', () => {
     options.tokens = ['usage', 'bg', 'standing-charge', 120]
-    expect(command(options)).toBe(14954)
+    expect(command(options)).toMatchSnapshot()
   })
 
   it('should return correct usage for variable plan', () => {
     options.tokens = ['usage', 'ovo', 'standard', 1000]
-    expect(command(options)).toBe(103855)
+    expect(command(options)).toMatchSnapshot()
   })
 
   it('should throw an error if wrong number of arguments were passed in', () => {
     options.tokens = ['usage']
-    const expectMessage = 'Error: format is "usage <supplier> <plan> <spend>"'
     let actualMessage = ''
     try { command(options) }
     catch (error) { actualMessage = error.toString() }
-    expect(actualMessage).toEqual(expectMessage)
+    expect(actualMessage).toMatchSnapshot()
   })
 
   it('should throw an error if an unrecognised supplier was passed in', () => {
     options.tokens = ['usage', 'banana', 'variable', 1000]
-    const expectMessage = 'Error: supplier "banana" not found in existing plans'
     let actualMessage = ''
     try { command(options) }
     catch (error) { actualMessage = error.toString() }
-    expect(actualMessage).toEqual(expectMessage)
+    expect(actualMessage).toMatchSnapshot()
   })
 
   it('should throw an error if an unrecognised plan was passed in', () => {
     options.tokens = ['usage', 'eon', 'banana', 1000]
-    const expectMessage = 'Error: plan "banana" not found in existing plans'
     let actualMessage = ''
     try { command(options) }
     catch (error) { actualMessage = error.toString() }
-    expect(actualMessage).toEqual(expectMessage)
+    expect(actualMessage).toMatchSnapshot()
   })
 
   it('should throw an error if invalid monthly spend was passed in ', () => {
     options.tokens = ['usage', 'eon', 'variable', 10.5]
-    const expectMessage = 'Error: spend "10.5" is not a positive integer amount'
     let actualMessage = ''
     try { command(options) }
     catch (error) { actualMessage = error.toString() }
-    expect(actualMessage).toEqual(expectMessage)
+    expect(actualMessage).toMatchSnapshot()
   })
 
   it('should throw an error if invalid supplier and plan were passed in', () => {
     options.tokens = ['usage', 'ovo', 'variable', 10]
-    const expectMessage = 'Error: plan matching query "{"supplier":"ovo","plan":"variable"}" not found in existing plans'
     let actualMessage = ''
     try { command(options) }
     catch (error) { actualMessage = error.toString() }
-    expect(actualMessage).toEqual(expectMessage)
+    expect(actualMessage).toMatchSnapshot()
   })
 
 })
